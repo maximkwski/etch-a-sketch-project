c16507086f5bf0a765907f29fa4aaeb3cf7121ee
Maksym <makkie.makowski@gmail.com>
2022-11-14 15:15:53 -0500

    add color change buttons, and reset btn

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let color = "black";
+let click = false;
+
 function generateBoard(size) {
-    const container = document.querySelector('.container');
+    let container = document.querySelector('.container');
     let squares = container.querySelectorAll('div');
     squares.forEach((div)=> div.remove());
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -9,8 +12,9 @@ function generateBoard(size) {
     for(i = 0; i < size**2; i++) {
         let square = document.createElement('div');
         square.classList.add('square');
-        square.style.backgroundColor = "salmon";
-        square.style.border = "1px solid black"
+        square.style.backgroundColor = "white";
+        square.style.border = "0.5px solid black";
+        square.addEventListener("mouseover", colorSquare);
         container.appendChild(square);
     };
 };
@@ -22,8 +26,34 @@ function changeSize(input) {
         generateBoard(input);    
     } else {
         console.log('wrong size');
-    }
+    };
+};
+
+function changeColor(choice) {
+    color = choice;
+};
+
+function colorSquare() {
+    if (click) {
+        if (color === 'rainbow') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = color;
+        }
+    };
+};
+
+function resetBoard() {
+    let container = document.querySelector('.container');
+    let squares = container.querySelectorAll('div');
+    squares.forEach((div)=> div.style.backgroundColor = 'white');
 }
+
+document.querySelector('body').addEventListener('click', ()=>{
+    click = !click;
+})
+
+
 
 
 
