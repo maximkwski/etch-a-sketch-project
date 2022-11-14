5ed877400f68d123694025a4a93abe04eeec0cda
Maksym <makkie.makowski@gmail.com>
2022-11-14 14:17:19 -0500

    add function to change grid size

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-const container = document.querySelector('.container');
-container.style.gridTemplateColumns = "repeat(16, 1fr)";
-container.style.gridTemplateRows = "repeat(16, 1fr)";
+function generateBoard(size) {
+    const container = document.querySelector('.container');
+    let squares = container.querySelectorAll('div');
+    squares.forEach((div)=> div.remove());
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
 
-for(i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.style.backgroundColor = "salmon";
-    square.style.border = "1px solid black"
-    container.appendChild(square);
+    for(i = 0; i < size**2; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.style.backgroundColor = "salmon";
+        square.style.border = "1px solid black"
+        container.appendChild(square);
+    };
 };
+
+generateBoard(16);
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        generateBoard(input);    
+    } else {
+        console.log('wrong size');
+    }
+}
+
+
 
 
 
